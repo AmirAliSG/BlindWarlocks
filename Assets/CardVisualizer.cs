1205f21c2c164167695fb366dca90a2cfebbfdba
AmirAliS <aas7mkn12@gmail.com>
2017-12-12 06:27:21 +0330

    far into hand UI and ready button interactablility, prepared data changed. no board and rules and network.

--- Assets/CardVisualizer.cs
+++ Assets/CardVisualizer.cs
@@ -7,32 +7,101 @@ using UnityEngine.UI;
 
 public class CardVisualizer : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
 {
+    
     [SerializeField]
     private Image background_;
+    [SerializeField]
+    private GameObject invis_child_;
     public Text name_;
-    private bool handmode_;
+    private bool ismoving_;
+    public bool ishandmode_;
     private CardData data_;
-	public void Visualize(CardData data,bool handmode)
+    private Vector3 firstplace_,childfirstplace_;
+    private Quaternion firstrotate_,childfirstrotation_;
+    public float scale_target_, upward_distance_, speed_;
+    public void Visualize(CardData data,bool ishandmode)
     {
         data_ = data;
         background_.sprite = data.Img;
         name_.text = data.name;
-        handmode_ = handmode;
+        ishandmode_ = ishandmode;
+        firstplace_ = transform.position;
+        firstrotate_ = transform.rotation;
+        childfirstplace_ = invis_child_.transform.position;
+        childfirstrotation_ = invis_child_.transform.rotation;
+        upward_distance_ = gameObject.GetComponent<RectTransform>().rect.height;
+        ismoving_ = false;
     }
-
+    private void SetBackInvisChild()
+    {
+        invis_child_.transform.position = childfirstplace_;
+        invis_child_.transform.rotation = childfirstrotation_;
+    }
+    private void SetBackTransform()
+    {
+        transform.SetPositionAndRotation(firstplace_, firstrotate_);
+        transform.localScale = Vector3.one;
+    }
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if(handmode_)
-        Debug.Log("Fuck Amirali");
+        if (ishandmode_)
+        {
+            firstplace_ = transform.position;
+            firstrotate_ = transform.rotation;
+            childfirstplace_ = invis_child_.transform.position;
+            childfirstrotation_ = invis_child_.transform.rotation;
+            transform.rotation = new Quaternion(0, 0, 0, 0);
+            SetBackInvisChild();
+            ismoving_ = true;
+        }
     }
+    public bool Move(Vector3 destination)
+    {
 
+        //float distance = speed* Time.deltaTime 
+
+        if (Vector3.Distance(transform.position, destination) < (float)speed_ * Time.deltaTime)
+        {
+            transform.position = destination;
+            SetBackInvisChild();
+            return true;
+        }
+        transform.Translate(speed_ * Time.deltaTime * (destination - transform.position));
+        if (transform.localScale.x < scale_target_)
+            transform.localScale = transform.localScale + Vector3.one * 0.1f;
+        else
+            transform.localScale = scale_target_*Vector3.one;
+        SetBackInvisChild();
+        return false;
+    }
+    private void Update()
+    {
+        if (ismoving_)
+            if (Move(firstplace_ + (Vector3.up * upward_distance_)))
+                ismoving_ = false;      
+    }
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("Really Fuck Amirali");
+        if (ishandmode_)
+        {
+            ismoving_ = false;
+            SetBackTransform();
+            SetBackInvisChild();
+            MainScript.instance_.SelectCardFromHand(data_, this);
+        }
+        else
+            MainScript.instance_.UnselectCard(data_, this);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        Debug.Log("Dude, Fuck Amirali FFS");
+        //Time.
+        //yield return new WaitForSeconds(1);
+        if (ishandmode_)
+        {
+            ismoving_ = false;
+            SetBackTransform();
+            SetBackInvisChild();
+        }
     }
 }
--- Assets/MainScript.cs
+++ Assets/MainScript.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.UI;
+//check all gameobject.find() names
 [System.Serializable]
 public class CardData {
     public Sprite Img;
@@ -21,9 +23,41 @@ public class Hand
 }
 public class Player
 {
-    public string name;
-    public List<CardData> hand;
+    public string name_;
+    public List<CardData> hand_;/// <summary>
+    /// /
+    /// </summary>
+    public CardData selected_card_;
+    public CardVisualizer show_selected_card_;
+    public DirectionShowerHandler selected_magic_direction_;
+    public DirectionShowerHandler selected_move_direction_;
+    public bool isready_;
+    public bool canready_;
+    public void CheckCanReady()
+    {
+        Button B = GameObject.Find("ReadyButton").GetComponent<Button>();
+        if (selected_magic_direction_ != null && selected_move_direction_ != null && selected_card_ != null)
+            B.interactable = true;
+        else
+            B.interactable = false;
+    }
+    public void SelectCard(CardData data,CardVisualizer cdref)
+    {
+        if (show_selected_card_ != null)
+            MainScript.instance_.UnselectCard(selected_card_, show_selected_card_);
+        show_selected_card_ = Object.Instantiate(GameObject.Find("CardDashboard").GetComponent<CardDashboardHandler>().card_prefab_, new Vector3(310, 280, 0), new Quaternion(0, 0, 0, 0), GameObject.Find("Canvas").transform).GetComponent<CardVisualizer>();
+        show_selected_card_.Visualize(data, false);
+        selected_card_ = data;
+        CheckCanReady();
+    }
+    public void UnselectCard()
+    {
+        selected_card_ = null;
+        GameObject.Destroy(show_selected_card_.gameObject);
+        CheckCanReady();
+    }
 }
+
 public class Pair<T1, T2>
 {
     private T1 item1_;
@@ -46,85 +80,90 @@ public class Pair<T1, T2>
         pItem2 = item2;
     }
 }
-
 public class MainScript : MonoBehaviour {
-    public UnityEngine.Events.UnityEvent Abdulah;
+    
     [UnityEngine.Serialization.FormerlySerializedAsAttribute("cards")]
-    public CardData[] cards_references_;
-    public GameObject card_prefab_;
+    
+    
+    public Player AmirAli;
+    public static MainScript instance_;
 
-    [MenuItem("fucker/ Change")]
+    [MenuItem("fucker/Change #&b")]
     static void hapalan()
     {
         int Canvas_Height = 502;
         int ismagic = 1;
-        int borderdistance = Canvas_Height/30;
-        //Abd.height
+        int borderdistance = Canvas_Height/20;
         string a = Selection.activeGameObject.name;
         //GameObject.Find("Canvas").GetComponent<RectTransform>.
-        int Radius = Canvas_Height/6;
+        int Radius = Canvas_Height/(2*4);
         if (a[0] == 'M')
             ismagic += 2;
         RectTransform ActiveRectTransform = Selection.activeGameObject.GetComponent<RectTransform>();
-        ActiveRectTransform.anchorMax = new Vector2(0.12f,1f - (float)(Radius + borderdistance )* ismagic/Canvas_Height);
-        ActiveRectTransform.anchorMin = new Vector2(0.12f,1f - (float)(Radius + borderdistance) * ismagic/Canvas_Height);
+        ActiveRectTransform.anchorMax = new Vector2(0.115f,1f - (float)(Radius + borderdistance )* ismagic/Canvas_Height);
+        ActiveRectTransform.anchorMin = new Vector2(0.115f,1f - (float)(Radius + borderdistance) * ismagic/Canvas_Height);
         //Selection.activeGameObject.transform.position += Vector3.forward * 10;
         
         int angel = ((int)a[a.Length - 2] - (int)'0') * 60;
-        
 
-        //Selection.activeGameObject.transform.Rotate(0, 0, angel);
-        Selection.activeGameObject.transform.rotation = Quaternion.Euler(0, 0, angel);
+
         Vector3 Direction = new Vector3(Mathf.Cos((angel - 90) * Mathf.PI / 180), Mathf.Sin((angel - 90) * Mathf.PI / 180), 0);
-        Selection.activeGameObject.GetComponent < RectTransform >().anchoredPosition = Radius * Direction;
-        
-        Debug.Log(Mathf.Sin((Mathf.PI*(angel-90))/180).ToString());
-        Debug.Log((angel).ToString());
+        if (a[a.Length - 2] == '7')
+        {
+            Direction = new Vector3(0, 0, 0);
+            angel = 0;
+        }
+        else
+            Selection.activeGameObject.transform.rotation = Quaternion.Euler(0, 0, angel+180);
+        ActiveRectTransform.anchoredPosition = Radius * Direction;
         
     }
     
     private Canvas canvas;
+    
     private List<Pair<CardData,CardVisualizer>> hand_;
-    public void MoveObject(Transform objecttransform,float speed,Vector3 destination)
-    {
-        
-        //float distance = speed* Time.deltaTime 
-          
-        //if(Vector3.Distance(objecttransform.position,destination)
-    }
+    
     // Use this for initialization
     void Start() {
-        hand_ = new List<Pair<CardData, CardVisualizer>>();
+        instance_ = this;
         canvas = FindObjectOfType<Canvas>();
-        ShowInitialHand(new List<CardData>(cards_references_));
+        AmirAli = new Player();
         
-        Debug.Log(transform.position.ToString());
-        //Selection.activeTransform.position;
     }
-    private void Update()
+    public void SelectDirectionShower(DirectionShowerHandler direction)
     {
-        //Debug.Log(Input.GetAxis("Horizontal").ToString());
-    }
-    private void ShowInitialHand(List<CardData> cards)
-    {
-        for (int i = 0; i < cards.Count; ++i)
-            Draw(cards[i]);
-        RefreshHandArrangements();
-    }
-
-    private void Draw(CardData card)
-    {
-        CardVisualizer vis = Instantiate(card_prefab_, canvas.transform).GetComponent<CardVisualizer>();
-        hand_.Add(new Pair<CardData, CardVisualizer>(card, vis));
-        vis.Visualize(card,true);
-    }
-
-    private void RefreshHandArrangements()
-    {
-        for(int i=0; i<hand_.Count; ++i)
+        direction.selected = true;
+        if (direction.ifmagic_)
         {
-            RectTransform trans = (RectTransform)hand_[i].pItem2.transform;
-            trans.anchoredPosition = new Vector2(i * (-trans.rect.width - 5), 0);
+            if (AmirAli.selected_magic_direction_ != null)
+                AmirAli.selected_magic_direction_.selected = false;
+            if (AmirAli.selected_magic_direction_ == direction)
+                AmirAli.selected_magic_direction_ = null;
+            else
+                AmirAli.selected_magic_direction_ = direction;
         }
+        else
+        {
+            if (AmirAli.selected_move_direction_ != null)
+                AmirAli.selected_move_direction_.selected = false;
+            if (AmirAli.selected_move_direction_ == direction)
+                AmirAli.selected_move_direction_ = null;
+            else
+                AmirAli.selected_move_direction_ = direction;
+        }
+        AmirAli.CheckCanReady();
+        gameObject.GetComponent<UnityEngine.UI.Selectable>().Select();
     }
- }
+    public void SelectCardFromHand(CardData data, CardVisualizer card)
+    {
+        AmirAli.SelectCard(data, card);
+        if (data.IsDefault == false)
+            CardDashboardHandler.instance_.Remove(data, card);
+    }
+    public void UnselectCard(CardData data,CardVisualizer cdvis)
+    {
+        if (data.IsDefault == false)
+            CardDashboardHandler.instance_.Draw(data);
+        AmirAli.UnselectCard();///this is required when only amirali have the not ishandmode_ shits
+    }
+}
